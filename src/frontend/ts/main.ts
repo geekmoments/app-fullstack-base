2bc0f0bc405a6fc1006d85a7ae63739ba4985482
geekmoments <cesar@geek-moments.com>
2021-06-14 13:29:09 -0500

    hello typscript clase 3

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,6 +1,6 @@
+class Main{
 
-function SayHello(){
-    let current_value = document.getElementById("textarea_1") as HTMLInputElement;
-    let new_value = "Hello world!!!" + "\n" + current_value.value;
-    document.getElementById("textarea_1").innerHTML = new_value;
+    public main(){
+        console.log("se ejcutó el metodo main");
+    }
 }
